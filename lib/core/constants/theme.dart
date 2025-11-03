2dbedda98d731c27beb9d2249470541d52c10a7b
gilangrizkiputra <gilangrizkiptra@gmail.com>
2025-11-03 20:07:06 +0700

    feat: set up theme

---
+++ lib/core/constants/theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = Color(0xFFFFFFFF);
+Color blackColor = Color(0xFF14193F);
+Color greyColor = Color(0xFFEAEAEA);
+Color lightBackgroundColor = Color(0xFFEAEAEA);
+Color darkBackgroundColor = Color(0xFF020518);
+Color purpleColor = Color(0xFF7965C1);
+Color greenColor = Color(0xFF0EFF2B);
+Color redColor = Color(0xFFFF0004);
+Color numberBackgroundColor = Color(0xFF1A1D2E);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
+TextStyle greyTextStyle = GoogleFonts.poppins(color: greyColor);
+TextStyle purpleTextStyle = GoogleFonts.poppins(color: purpleColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
